d40a8d97d5061f276946a6e1d9dfcefe885f7838
Kamil Grabowski <kg303164@student.polsl.pl>
2024-12-03 11:03:20 +0100

    Dodaj pliki projektów.

---
+++ Advent_of_code_2024/Day 1/Task1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Advent_of_code_2024.Day_1
+{
+    public class Task1
+    {
+        private List<int> FirstNumbers = new List<int>();
+        private List<int> SecondNumbers = new List<int>();
+        private void ReadInput()
+        {
+            using (var fileStream = File.OpenRead(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../../Day 1", "input.txt")))
+            using (var streamReader = new StreamReader(fileStream))
+            {
+                String line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    var splitedString = line.Split(" ");
+                    FirstNumbers.Add(int.Parse(splitedString[0]));
+                    SecondNumbers.Add(int.Parse(splitedString[splitedString.Length-1]));
+                }
+            }
+        }
+        public void Solve()
+        {
+            ReadInput();
+            int similarity = 0;
+            foreach (var number in FirstNumbers)
+            {
+                int count = SecondNumbers.Count(x => x == number);
+                similarity += count * number;
+            }
+            Console.WriteLine($"Similarity: {similarity}");
+
+            FirstNumbers.Sort();
+            SecondNumbers.Sort();
+
+            int sum = FirstNumbers.Zip(SecondNumbers, (first, second) => Math.Abs(first - second)).Sum();
+            Console.WriteLine($"Sum: {sum}");
+
+
+        }
+
+        
+    }
+}
---
+++ Advent_of_code_2024/Program.cs
@@ -0,0 +1,28 @@
+﻿using Advent_of_code_2024.Day_1;
+using System;
+
+namespace Advent_of_code_2024 // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string x = Console.ReadLine();
+
+            switch (x)
+            {
+                case "1":
+                    Task1 task1 = new Task1();
+                    task1.Solve();
+                    break;
+                //case 2:
+                //    Task2 task2 = new Task2();
+                //    task2.Solve();
+                //    break;
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+        }
+    }
+}
